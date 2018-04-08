872eef3eb6c9c9fa3408b4b5a5a7115cb9334e25
YFQ <qiyf@mit.edu>
2018-04-08 16:01:07 -0400

    v0.20

--- contact_map/parallel_cmap.py
+++ contact_map/parallel_cmap.py
@@ -14,27 +14,30 @@ from combineMaps import combine_cmaps
 if __name__ == '__main__':
 
 	clus_opt=input('Computing clusters available?[y/n]')
+
+	if len(sys.argv) == 1:
+		# celltype = 'Gm12878'
+		# runnum = 8
+		# chrom_lst = [1]
+		pass
+	else:
+		celltype = sys.argv[1]
+		runnum = int(sys.argv[2])
+		chrom_lst = sys.argv[3:]
+		chrom_lst = map(eval, chrom_lst)
+
 	if clus_opt == 'y':
-		if len(sys.argv) == 1:
-			# celltype = 'Gm12878'
-			# runnum = 8
-			# chrom_lst = [1]
-			pass
-		else:
-			celltype = sys.argv[1]
-			runnum = int(sys.argv[2])
-			chrom_lst = sys.argv[3:]
-			chrom_lst = map(eval, chrom_lst)
+	# job_name = 'cmap'										# name of the jobs running cmaps
+	# ptn_name = 'sched_mit'									# name of the partition name
+	# 														# need to be changed accordingly
+		for chromid in chrom_lst:
+			for runid in xrange(runnum):
+				processingJobScript(celltype,chromid,runid,job_name,ptn_name)
+																# prepare the job script for calculating cmaps
+		check_status()											# check the job status
 
-	job_name = 'cmap'										# name of the jobs running cmaps
-	ptn_name = 'sched_mit'									# name of the partition name
-															# need to be changed accordingly
-
-	for chromid in chrom_lst:
-		for runid in xrange(runnum):
-			processingJobScript(celltype,chromid,runid,job_name,ptn_name)
-															# prepare the job script for calculating cmaps
-	check_status()											# check the job status
+	else:
+		calMapLocal(celltype,chromid,runid)
 
 	for chromid in chrom_lst:
 		combine_cmaps(celltype,chromid,runnum)		# combine the parallel cmaps to ensemble average
