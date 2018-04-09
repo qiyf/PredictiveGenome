bb02db114ba59ae81ac1d5b0bace3c5db9610a07
YFQ <qiyf@mit.edu>
2018-04-09 11:10:36 -0400

    v0.20

--- contact_map/src/PYTHON/getSettings.py
+++ contact_map/src/PYTHON/getSettings.py
@@ -6,3 +6,26 @@ def getSettings(argv):
 	try:
 		opts,args = getopt.getopt(args,'hC:r:c:',\
 								['Cell=','runnum=','chrom='])
+	except getopt.GetoptError:
+		print('''
+>>>> Error: parallel_cmap.py -C <Celltype> -r <run number> -c <chromosome id>
+        or: parallel_cmap.py --Cell <Celltype> --runnum <run number> --chrom <chromosome id>
+
+      type: parallel_cmap.py -h 
+      		to see the manual.''')
+		sys.exit(2)
+
+	for opt,arg in opts:
+		if opt=='-h':
+			print('''
+>>>> Error: parallel_cmap.py -C <Celltype> -r <run number> -c <chromosome id>
+        or: parallel_cmap.py --Cell <Celltype> --runnum <run number> --chrom <chromosome id>''')
+			sys.exit()
+		elif opt in ('-C','--Cell'):
+			Celltype = arg
+		elif opt in ('-r','--runnum'):
+			runnum = arg
+		elif opt in ('-c','--chrom'):
+			chromid = arg
+
+	return Celltype,runnum,chromid
