8ed285915df921f4718a2d23e802fc686b042c8c
Bin Zhang <binz@mit.edu>
2018-04-21 01:48:43 -0400

    move settings to the gencs file

--- src/chromatinState.py
+++ src/chromatinState.py
@@ -97,36 +97,3 @@ class chromatinState(object):
 	print('''   > Chromatin state for %s, chromosome %d is successfully generated.'''\
 																	%(self.celltype,self.chrId))
 
-def getSettings(argv):
-
-# ---- default values ---- #
-	Celltype = 'Gm12878';chrom_lst=[1];
-# ------------------------ #
-	
-	try:
-		opts,args = getopt.getopt(argv,'hC:c:',\
-								['Cell=','chrom='])
-		for opt,arg in opts:
-			if opt=='-h':
-				print('''
->>>> Options: genChromState.py -C <Celltype> -c <chromosome id>
-          or: genChromState.py --Cell <Celltype> --chrom <chromosome id>
-''')
-				sys.exit()
-			elif opt in ('-C','--Cell'):
-				Celltype = arg
-			elif opt in ('-c','--chrom'):
-				chrom1st	= [int(arg)]
-				chrom2te	= map(eval, args)
-				chrom_lst	= chrom1st+chrom2te
-		print('''
->>>> Calculating chromatin states of %s ......'''%Celltype)
-		return Celltype,chrom_lst
-
-	except getopt.GetoptError:
-		print('''
->>>> [Warning] Error in setting options.
-   > To see the manual and change the settings:
-     python genChromState.py -h
-''')
-		sys.exit()
